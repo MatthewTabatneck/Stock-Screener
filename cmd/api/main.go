fc7c3daff697c79e1c19e6c7ac04d51053a3612e
MatthewTabatneck <mmtabat3@gmail.com>
2025-10-09 23:32:55 -0400

    csv is accepted and parsed

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"os"
+
+	"github.com/MatthewTabatneck/stock-screener/internal/screener"
 )
 
+// func main() {
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintln(w, "ok")
+// 	})
+// 	port := "8080"
+// 	log.Printf("listening on :%s", port)
+// 	log.Fatal(http.ListenAndServe(":"+port, mux))
+// }
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "ok")
-	})
-	port := "8080"
-	log.Printf("listening on :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	file, err := os.Open("sp500_tickers.csv")
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return
+	}
+	defer file.Close()
+
+	tickers, err := screener.LoadtickersCSV(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(tickers)
+
 }
